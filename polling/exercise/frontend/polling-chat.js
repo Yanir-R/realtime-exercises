18cd8007cc344f60d73f9197d2be4825554f089a
Sheba467 <lowyouone@gmail.com>
2022-01-21 21:41:33 +0200

    adding requestAnimationFrame instead of setTimeout

--- polling/exercise/frontend/polling-chat.js
+++ polling/exercise/frontend/polling-chat.js
@@ -1,19 +1,16 @@
 const chat = document.getElementById("chat");
 const msgs = document.getElementById("msgs");
 
-// let's store all current messages here
+// this will hold all the most recent messages
 let allChat = [];
 
-// the interval to poll at in milliseconds
 const INTERVAL = 3000;
 
-// a submit listener on the form in the HTML
 chat.addEventListener("submit", function (e) {
   e.preventDefault();
   postNewMsg(chat.elements.user.value, chat.elements.text.value);
   chat.elements.text.value = "";
 });
-
 async function postNewMsg(user, text) {
   const data = {
     user,
@@ -39,30 +36,36 @@ async function postNewMsg(user, text) {
 
 async function getNewMsgs() {
   let json;
-  try{
-    const res = await fetch('/poll');
+  try {
+    const res = await fetch("/poll");
     json = await res.json();
-  }catch (e){
-    //backoff code
-    console.error('polling error', e)
+  } catch (e) {
+    // back off could would go here
+    console.error("polling error", e);
   }
   allChat = json.msg;
-  render()
-  setTimeout(getNewMsgs, INTERVAL);
+  render();
 }
 
 function render() {
-  // as long as allChat is holding all current messages, this will render them
-  // into the ui. yes, it's inefficent. yes, it's fine for this example
+  // todo
   const html = allChat.map(({ user, text, time, id }) =>
     template(user, text, time, id)
   );
   msgs.innerHTML = html.join("\n");
 }
 
-// given a user and a msg, it returns an HTML string to render to the UI
 const template = (user, msg) =>
   `<li class="collection-item"><span class="badge">${user}</span>${msg}</li>`;
 
-// make the first request
-getNewMsgs();
+let timeToMakeNextRequest = 0;
+
+async function rafTimer(time){
+  if(timeToMakeNextRequest <= time){
+    await getNewMsgs();
+    timeToMakeNextRequest = time + INTERVAL;
+  }
+  requestAnimationFrame(rafTimer);
+}
+
+requestAnimationFrame(rafTimer);
